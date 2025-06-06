a9fcb2c5df59712a9742e7b5e3a5181211567f7f
htevilili1991 <htevilili@gmail.com>
2025-06-06 17:38:30 +1100

    Fix error

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx','resources/js/pages/settings/Profile.tsx'],
+            input: ['resources/css/app.css', 'resources/js/app.tsx','resources/js/pages/settings/profile.tsx'],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
