40469e927e67af6323444c1459437eaf7b3a0dd9
htevilili1991 <htevilili@gmail.com>
2025-06-09 16:38:06 +1100

    minor fixes

--- routes/settings.php
+++ routes/settings.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Settings\PasswordController;
 use App\Http\Controllers\Settings\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/settings', [ProfileController::class, 'index'])->name('settings.index');
